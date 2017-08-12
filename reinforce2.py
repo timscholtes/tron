7a831b6cb70f7ae8aeb26f14470704b172931a49
Tim Scholtes <timscholtes@tims-mbp.home>
2017-08-12 15:32:35 +0100

    rejigged to create dataset, and then train on it

--- reinforce2.py
+++ reinforce2.py
@@ -6,6 +6,8 @@ import itertools
 import numpy as np
 import pickle
 from game import game
+import tensorflow as tf
+import multiprocessing as mp
 
 class randomBot:
 
@@ -17,42 +19,123 @@ class randomBot:
 		rm = random.randint(0,len(lmoves)-1)
 		return lmoves[rm]
 
-class MonteCarloExplore(game):
-	def __init__(self,N,K, p1,p2):
-		self.size = 21
-		self.N = N
-		self.K = K
-		self.p1 = p1
-		self.p2 = p2
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-	def start_board_generator(self):
-		boards = []
-		for n in range(self.N):
-			print n
-			game_boards = self.play_game_record_boards(self.p1,self.p2)
-			boards.append(self.deepish_copy(random.choice(game_boards)))
-		return boards
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
-	def rollout_player(self,start_boards):
-		results = []
-		for n,b in enumerate(start_boards):
-			print n
-			mini_res = []
-			for k in range(self.K):
-				res = self.play_game_from_board(b,self.p1,self.p2)
-				if res != -1:
-					mini_res.append(res)
-			
-			if len(mini_res) == 0:
-				results.append(0.5)
-			else:
-				results.append(np.mean(mini_res))
-		return results
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
-	def generate_board_results(self):
-		boards = self.start_board_generator()
-		results = self.rollout_player(boards)
-		return [b['cells'] for b in boards], results
+
+# class MonteCarloExplore(game):
+# 	def __init__(self,N,K, p1,p2):
+# 		size = 21
+# 		N = N
+# 		K = K
+# 		p1 = p1
+# 		p2 = p2
+
+
+g = game()
+
+LEFT  = np.array([0,-1])
+RIGHT = np.array([0,1])
+UP    = np.array([1,0])
+DOWN  = np.array([-1,0])
+
+p1 = randomBot()
+p2 = randomBot()
+
+N = 1000
+K = 500
+
+def start_board_generator():
+	boards = []
+	for n in range(N):
+		game_boards = g.play_game_record_boards(p1,p2)
+		boards.append(g.deepish_copy(random.choice(game_boards)))
+	return boards
+
+def rollout_player(start_boards):
+	results = []
+	for n,b in enumerate(start_boards):
+		mini_res = []
+		for k in range(K):
+			res = g.play_game_from_board(b,p1,p2)
+			if res != -1:
+				mini_res.append(res)
+		
+		if len(mini_res) == 0:
+			results.append(0.5)
+		else:
+			results.append(np.mean(mini_res))
+	return results
+
+
+def rollout_single_player(start_board):
+
+	mini_res = []
+	for k in range(K):
+		res = g.play_game_from_board(start_board,p1,p2)
+		if res != -1:
+			mini_res.append(res)
+		
+	if len(mini_res) == 0:
+		return 0.5
+	else:
+		return np.mean(mini_res)
+
+
+def generate_board_results():
+	boards = start_board_generator()
+	results = rollout_player(boards)
+	return np.stack([b['cells'] for b in boards],axis=0), results
+
+def generate_single_board_res():
+
+	b = start_board_generator()[0]
+	res = rollout_single_player(b)
+	return b['cells'],res
+
+def outer_write_func(num_datasets,runs_per_dataset):
+	for nd in range(num_datasets):
+		inner_write_func(nd,runs_per_dataset)
+
+
+def inner_write_func(nd):
+	print 'starting ',nd
+	writer = tf.python_io.TFRecordWriter('data/board_outcome_'+str(nd)+'.tfrecords')
+
+	for rd in range(10):
+		print rd
+		boards,results = generate_board_results()
+
+		for b, r in zip(boards,results):
+			r = np.float64(r)
+			b2 = b.tolist()
+			b3 = [x for y in b2 for x in y]
+			example = tf.train.Example(features=tf.train.Features(
+				feature = {
+				'boards': _float_feature(b3),
+				'results': _float_feature([r])
+				}))
+			writer.write(example.SerializeToString())
+	writer.close()
+
+def write_func_mp(nc,num_datasets,runs_per_dataset):
+
+	pool = mp.Pool(processes=nc)
+
+	for nd in range(num_datasets):
+		pool.apply_async(inner_write_func,[nd])
+		#pool.map(inner_write_func,range(nd))
+
+	pool.close()
+	pool.join()
+
+
 
 
 
@@ -63,27 +146,34 @@ class MonteCarloExplore(game):
 if __name__ == '__main__':
 
 
-	g = game()
+	write_func_mp(4,16,50)
 
 
-	LEFT  = np.array([0,-1])
-	RIGHT = np.array([0,1])
-	UP    = np.array([1,0])
-	DOWN  = np.array([-1,0])
+# 	g = game()
 
-	p1 = randomBot()
-	p2 = randomBot()
+# 	LEFT  = np.array([0,-1])
+# 	RIGHT = np.array([0,1])
+# 	UP    = np.array([1,0])
+# 	DOWN  = np.array([-1,0])
 
-	MC = MonteCarloExplore(128*100,10,p1,p2)
+# 	p1 = randomBot()
+# 	p2 = randomBot()
+
+# 	MC = MonteCarloExplore(100,20,p1,p2)
+# 	print 'starting MP:'
+# 	#
+# 	MC.write_func_mp(4,10,10)
+	#MC.outer_write_func(10,10)
+	
 
 
-	boards,results = MC.generate_board_results()
+	# boards,results = MC.generate_board_results()
 
-	f = open('data/train_boards.pickle', 'wb')
-	save = {
-	'boards': boards,
-	'results': results
-	}
-	pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
-	f.close()
+	# f = open('data/valid_boards.pickle', 'wb')
+	# save = {
+	# 'boards': boards,
+	# 'results': results
+	# }
+	# pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+	# f.close()
 
--- valueNetwork.py
+++ valueNetwork.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import numpy as np
 import os
 import pickle
+import reinforce2
+import threading
+import time
 
 LEFT  = np.array([0,-1])
 RIGHT = np.array([0,1])
@@ -9,182 +12,210 @@ UP    = np.array([1,0])
 DOWN  = np.array([-1,0])
 DEVICE = '/cpu:0'
 
-class valueNetwork:
 
-	def __init__(self,board_size,restore_id=None,
-		valid_boards=None,valid_labels=None):
-		self.patch_sizes= [8,4,4]
-		self.depths = [32,64,64]
-		self.strides = [4,2,1]
-		self.num_hidden1 = 512
-		self.num_hidden2 = 256
-		self.num_labels = 1
-		self.board_size = board_size
-		self.batch_size = 512
-		self.learn_rate = 0.01
-		self.L2_penalty = 0.01
-		self.graph = tf.Graph()
+board_size = 21
+patch_sizes= [8,4,4]
+depths = [32,64,64]
+strides = [4,2,1]
+num_hidden1 = 512
+num_hidden2 = 256
+num_labels = 1
+board_size = board_size
+batch_size = 50
+learn_rate = 0.005
+L2_penalty = 0.001
+num_epochs = 100
+#N = 2000
 
-		with self.graph.as_default():
-
-			self.batch_boards = tf.placeholder(
-				tf.float32,shape=(self.batch_size,self.board_size,self.board_size,1))
-			self.batch_labels = tf.placeholder(tf.int32)
-			if valid_boards is not None:
-				valid_boards = tf.constant(valid_boards)
-				valid_labels = tf.constant(valid_labels)
-
-			# define weights, biases of each layer
-			self.w1 = tf.Variable(tf.truncated_normal(
-				[self.patch_sizes[0], self.patch_sizes[0], 1, self.depths[0]], stddev=0.1))
-			self.b1 = tf.Variable(tf.zeros([self.depths[0]]))
-
-			self.w2 = tf.Variable(tf.truncated_normal(
-				[self.patch_sizes[1], self.patch_sizes[1], self.depths[0], self.depths[1]], stddev=0.1))
-			self.b2 = tf.Variable(tf.constant(1.0,shape=[self.depths[1]]))
-
-			self.w3 = tf.Variable(tf.truncated_normal(
-				[self.patch_sizes[2], self.patch_sizes[2], self.depths[1], self.depths[2]], stddev=0.1))
-			self.b3 = tf.Variable(tf.constant(1.0,shape=[self.depths[2]]))
-
-			self.w4 = tf.Variable(tf.truncated_normal(
-				# [self.board_size // 4 * self.board_size // 4 * self.depth, self.num_hidden], stddev=0.1))
-				[6*6*16, self.num_hidden1], stddev=0.1))
-			self.b4 = tf.Variable(tf.constant(1.0,shape=[self.num_hidden1]))
-
-			self.w5 = tf.Variable(tf.truncated_normal(
-				[self.num_hidden1, self.num_hidden2], stddev=0.1))
-			self.b5 = tf.Variable(tf.constant(1.0,shape=[self.num_hidden2]))
-
-			self.w6 = tf.Variable(tf.truncated_normal(
-				[self.num_hidden2, self.num_labels], stddev=0.1))
-			self.b6 = tf.Variable(tf.constant(1.0,shape=[self.num_labels]))
-
-			regularizers = tf.nn.l2_loss(self.w1) + tf.nn.l2_loss(self.w2) + \
-				 tf.nn.l2_loss(self.w3) + tf.nn.l2_loss(self.w4) + \
-				 tf.nn.l2_loss(self.w5) +tf.nn.l2_loss(self.w6)
+valid_boards = None
+valid_labels = None
 
 
-			logits = self.model_train(self.train_boards)
-			# loss = tf.reduce_mean(
-			# 	tf.nn.softmax_cross_entropy_with_logits(labels=train_labels,logits=logits))
+######
 
-			# action_onehot = tf.one_hot(self.train_labels,4)
-			# action_prob = logits * action_onehot
-			# #action_prob = tf.gather(logits,self.train_labels)
-			# loss = tf.reduce_mean( - self.train_targets * tf.log(action_prob + 1e-13))
-
-			self.loss1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-				labels=self.train_labels,logits=logits))
-
-			loss = tf.reduce_mean(self.loss1+self.L2_penalty*regularizers)
-
-			if valid_boards is not None:
-				valid_logits = self.model(valid_boards)
-				self.valid_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-					labels=valid_labels,logits=valid_logits))
-
-			self.optimizer = tf.train.GradientDescentOptimizer(self.learn_rate).minimize(loss)			
-
-			self.sess = tf.Session(graph=self.graph)
-			
-			self.saver = tf.train.Saver()
-			if restore_id is not None:
-				#print 'restoring from: ',"/models/model"+str(restore_id)+".ckpt"
-				self.saver.restore(self.sess,"./models/model"+str(restore_id)+".ckpt")
-			else:
-				print 'initializing'
-				init = tf.global_variables_initializer()
-				self.sess.run(init)
-			
-
-	def model(self,cells):
-		with tf.device(DEVICE):
-			conv = tf.nn.conv2d(cells, self.w1, [1, 4, 4, 1], padding='SAME')
-			hidden = tf.nn.relu(conv + self.b1)
-			conv = tf.nn.conv2d(hidden, self.w2, [1, 2, 2, 1], padding='SAME')
-			hidden = tf.nn.relu(conv + self.b2)
-			conv = tf.nn.conv2d(hidden, self.w3, [1, 1, 1, 1], padding='SAME')
-			hidden = tf.nn.relu(conv + self.b3)
-
-			shape = hidden.get_shape().as_list()
-			reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
-			hidden1 = tf.nn.relu(tf.matmul(reshape, self.w4) + self.b4)
-			hidden2 = tf.nn.relu(tf.matmul(hidden1, self.w5) + self.b5)
-			out =  tf.nn.softmax(tf.matmul(hidden2, self.w6) + self.b6)
-		return out
-
-	def model_train(self,cells):
-		with tf.device(DEVICE):
-			conv = tf.nn.conv2d(cells, self.w1, [1, 4, 4, 1], padding='SAME')
-			hidden = tf.nn.relu(conv + self.b1)
-			conv = tf.nn.conv2d(hidden, self.w2, [1, 2, 2, 1], padding='SAME')
-			hidden = tf.nn.relu(conv + self.b2)
-			conv = tf.nn.conv2d(hidden, self.w3, [1, 1, 1, 1], padding='SAME')
-			hidden = tf.nn.relu(conv + self.b3)
-
-			shape = hidden.get_shape().as_list()
-			reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
-			hidden1 = tf.nn.relu(tf.matmul(reshape, self.w4) + self.b4)
-			hidden_dropped1 = tf.nn.dropout(hidden1,0.5)
-			hidden2 = tf.nn.relu(tf.matmul(hidden_dropped1, self.w5) + self.b5)
-			hidden_dropped2 = tf.nn.dropout(hidden2,0.5)
-
-			out =  tf.nn.softmax(tf.matmul(hidden_dropped2, self.w6) + self.b6)
-		return out
+filenames = ['data/board_outcome_'+str(i)+'.tfrecords' for i in range(12)]
+filename_queue = tf.train.string_input_producer(string_tensor = filenames,
+	num_epochs = num_epochs,shuffle = True)
 
 
-	def update_pars(self,train_boards,train_labels,N, batch_size):
-		print 'Step:','Train_loss','Valid_loss'
-		for n in N:
-			offset = (n * batch_size) % (self.train_labels.shape[0] - batch_size)
-			# Generate a minibatch.
-			batch_boards = self.train_boards[offset:(offset + batch_size), :]
-			batch_labels = self.train_labels[offset:(offset + batch_size), :]
-			feed_dict = {self.batch_boards: batch_boards,
-			self.batch_labels: batch_labels}
-			self.sess.run(self.optimizer,feed_dict)
+def read_and_decode(filename_queue):
+	reader = tf.TFRecordReader()
+	_, serialized_example = reader.read(filename_queue)
+	features = tf.parse_single_example(
+		serialized_example,
+		# Defaults are not specified since both keys are required.
+		features={
+		'boards': tf.FixedLenFeature([board_size,board_size], tf.float32),
+		'results': tf.FixedLenFeature([1], tf.float32),
+		},
+		name= 'board_res')
+	print features
+	#board = tf.decode_raw(features['board_raw'],tf.float32)
+	#result = tf.decode_raw(features['result_raw'],tf.float32)
+	board = features['boards']
+	result = features['results']
+	board = tf.reshape(board,(board_size,board_size,1))
+	#result = tf.cast(result,tf.float32)
+	result.set_shape([1])
+	return board,result
+
+board, result = read_and_decode(filename_queue)
+
+board_batch, result_batch = tf.train.shuffle_batch([board,result],
+	batch_size = batch_size, num_threads = 2, capacity = 10000 + 3*batch_size,
+	min_after_dequeue = 10000)
+
+print board_batch, result_batch
+######
+
+def model(cells):
+	with tf.device(DEVICE):
+		conv = tf.nn.conv2d(cells, w1, [1, 4, 4, 1], padding='SAME')
+		hidden = tf.nn.relu(conv + b1)
+		conv = tf.nn.conv2d(hidden, w2, [1, 2, 2, 1], padding='SAME')
+		hidden = tf.nn.relu(conv + b2)
+		conv = tf.nn.conv2d(hidden, w3, [1, 1, 1, 1], padding='SAME')
+		hidden = tf.nn.relu(conv + b3)
+
+		shape = hidden.get_shape().as_list()
+		reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+		hidden1 = tf.nn.relu(tf.matmul(reshape, w4) + b4)
+		hidden2 = tf.nn.relu(tf.matmul(hidden1, w5) + b5)
+		out = tf.nn.sigmoid(tf.matmul(hidden2, w6) + b6)
+	return out
+
+def model_train(cells):
+	with tf.device(DEVICE):
+		conv = tf.nn.conv2d(cells, w1, [1, 4, 4, 1], padding='SAME')
+		hidden = tf.nn.relu(conv + b1)
+		conv = tf.nn.conv2d(hidden, w2, [1, 2, 2, 1], padding='SAME')
+		hidden = tf.nn.relu(conv + b2)
+		conv = tf.nn.conv2d(hidden, w3, [1, 1, 1, 1], padding='SAME')
+		hidden = tf.nn.relu(conv + b3)
+
+		shape = hidden.get_shape().as_list()
+		reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+		hidden1 = tf.nn.relu(tf.matmul(reshape, w4) + b4)
+		hidden_dropped1 = tf.nn.dropout(hidden1,0.5)
+		hidden2 = tf.nn.relu(tf.matmul(hidden_dropped1, w5) + b5)
+		hidden_dropped2 = tf.nn.dropout(hidden2,0.5)
+
+		out = tf.nn.sigmoid(tf.matmul(hidden_dropped2, w6) + b6)
+	return out
+
+
+
+
+
+# valid_boards = tf.constant(valid_boards)
+# valid_labels = tf.constant(valid_labels)
+
+# define weights, biases of each layer
+w1 = tf.Variable(tf.truncated_normal(
+	[patch_sizes[0], patch_sizes[0], 1, depths[0]], stddev=0.1))
+b1 = tf.Variable(tf.zeros([depths[0]]))
+
+w2 = tf.Variable(tf.truncated_normal(
+	[patch_sizes[1], patch_sizes[1], depths[0], depths[1]], stddev=0.01))
+b2 = tf.Variable(tf.constant(1.0,shape=[depths[1]]))
+
+w3 = tf.Variable(tf.truncated_normal(
+	[patch_sizes[2], patch_sizes[2], depths[1], depths[2]], stddev=0.001))
+b3 = tf.Variable(tf.constant(1.0,shape=[depths[2]]))
+
+w4 = tf.Variable(tf.truncated_normal(
+	# [board_size // 4 * board_size // 4 * depth, num_hidden], stddev=0.1))
+	[6*6*16, num_hidden1], stddev=0.001))
+b4 = tf.Variable(tf.constant(1.0,shape=[num_hidden1]))
+
+w5 = tf.Variable(tf.truncated_normal(
+	[num_hidden1, num_hidden2], stddev=0.001))
+b5 = tf.Variable(tf.constant(1.0,shape=[num_hidden2]))
+
+w6 = tf.Variable(tf.truncated_normal(
+	[num_hidden2, num_labels], stddev=0.001))
+b6 = tf.Variable(tf.constant(1.0,shape=[num_labels]))
+
+regularizers = tf.nn.l2_loss(w1) + tf.nn.l2_loss(w2) + \
+	 tf.nn.l2_loss(w3) + tf.nn.l2_loss(w4) + \
+	 tf.nn.l2_loss(w5) +tf.nn.l2_loss(w6)
+
+
+logits = model_train(board_batch)
+# loss1 = tf.nn.softmax_cross_entropy_with_logits(
+# 	labels=board_result_batch[1],
+# 	logits=logits)
+
+loss1 = tf.reduce_mean(tf.nn.l2_loss(logits - result_batch))
+
+loss = tf.reduce_mean(loss1+L2_penalty*regularizers)
+
+if valid_boards is not None:
+	valid_logits = model(valid_boards)
+	valid_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+		labels=valid_labels,logits=valid_logits))
+
+#optimizer = tf.train.GradientDescentOptimizer(learn_rate).minimize(loss)			
+optimizer = tf.train.MomentumOptimizer(learn_rate,0.9).minimize(loss)			
+# qr = tf.train.QueueRunner(queue,[enqueue_op]*4)
+
+tf.summary.scalar('train_loss',loss)
+merged = tf.summary.merge_all()
+summary_writer = tf.summary.FileWriter('./checkpoints')
+
+init_op = tf.group(tf.global_variables_initializer(),
+	tf.local_variables_initializer())
+with tf.Session() as sess:
+	coord = tf.train.Coordinator()
+	sess.run(init_op)
+
+	threads = tf.train.start_queue_runners(sess=sess,coord=coord)
+
+	print 'Step:','Train_loss'
+	n = 0
+	loss_list = []
+	try:
+#		for n in range(N):
+#			if coord.should_stop():
+#				break
+		while not coord.should_stop():
+			n += 1
+			# b,r = sess.run([board,result])
+			# print b,r
+
+
+			_ ,l,summary= sess.run([optimizer,loss,merged])
+			summary_writer.add_summary(summary,n)
+			loss_list.append(l)
 			if n % 100 == 0:
-				train_loss = self.loss1.eval()
-				valid_loss = self.valid_loss.eval()
-				print n, train_loss, valid_loss
-		pass
+				l_mean = np.mean(loss_list)
+				print n, l_mean, time.time()
+				
+				loss_list = []
 
 
-	def save_session(self,id):
-		save_path = self.saver.save(self.sess,"./models/model"+str(id)+".ckpt")
-		print("Model saved in file: %s" % save_path)
-		pass
+	except tf.errors.OutOfRangeError:
+		print('Done training -- epoch limit reached')
+	finally:
+		# When done, ask the threads to stop.
+		coord.request_stop()
 
-	def manual_restore(self,restore_id):
-		print 'restoring from: ',"./models/model"+str(restore_id)+".ckpt"
-		self.saver.restore(self.sess,"./models/model"+str(restore_id)+".ckpt")
-		pass
-
+	coord.request_stop()
+	coord.join(threads)
 
 
 
-if __name__ == '__main__':
+def save_session(id):
+	save_path = saver.save(sess,"./models/model"+str(id)+".ckpt")
+	print("Model saved in file: %s" % save_path)
+	pass
 
-	import game
-	import numpy as np
-	LEFT  = np.array([0,-1])
-	RIGHT = np.array([0,1])
-	UP    = np.array([1,0])
-	DOWN  = np.array([-1,0])
+def manual_restore(restore_id):
+	print 'restoring from: ',"./models/model"+str(restore_id)+".ckpt"
+	saver.restore(sess,"./models/model"+str(restore_id)+".ckpt")
+	pass
 
-	with open('data/train_boards.pickle', 'rb') as f:
-		data = pickle.load(f)
-		train_boards = data['boards']
-		train_results = data['results']
-		del data
 
-	with open('data/valid_boards.pickle', 'rb') as f:
-		data = pickle.load(f)
-		valid_boards = data['boards']
-		valid_results = data['results']
-		del data
 
-	VN = valueNetwork(21,None,valid_boards,valid_results)
 
 
